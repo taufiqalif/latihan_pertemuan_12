5b27fc283a1adc667ee53d9993a1b7b21e5efb46
taufiqalif <aliftaufiq4@gmail.com>
2020-12-07 12:50:18 +0700

    latihan dengan string

---
+++ README.md
@@ -0,0 +1,15 @@
+# String
+
+
+## Tetapkan String ke Variabel
+Menetapkan **string** ke **variabel** dilakukan dengan nama variabel diikuti dengan tanda sama dengan dan string:
+
+	var1 = 'hello world'
+	var2 = "python programing"
+
+	print(("var1[0]: "), var1[0])
+	print(("var2[1:5]: "), var2[1:5])
+
+dan akan menghasilkan output sebagai berikut:
+
+![01.png](/gambar/01.png)
---
+++ latihan_pertemuan_12.py
@@ -0,0 +1,30 @@
+#var1 = 'hello world'
+#var2 = "python programing"
+
+#print(("var1[0]: "), var1[0])
+#print(("var2[1:5]: "), var2[1:5])
+
+
+#var1 = 'hello world'
+
+#print("update string : - ", var1[:6] + 'python')
+
+print("nama saya %s, umur saya adalah %s tahun" % ('taufiq alif rahman', 22))
+
+# print(r'C:\\taufiq')
+
+
+#txt = 'hello world'
+
+# print(len(txt))
+# print(txt[10])
+# print(txt[2:5])
+#print(txt[:5] + 'world')
+# print(txt.upper())
+# print(txt.lower())
+#print(txt.replace("h", "J"))
+
+
+#umur = 24
+#txt = 'hallo, nama saya john, dan umur saya adalah {} tahun'
+# print(txt.format(umur))
